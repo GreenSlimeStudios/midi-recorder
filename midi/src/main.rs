634a0c27d56e7190edf7baa924425c47256ee8d6
Max Berliński <maxber.gdynia@gmail.com>
2022-06-30 13:09:07 +0200

    removed unused dependencies and code reformat

--- midi/src/main.rs
+++ midi/src/main.rs
@@ -1,44 +1,45 @@
 extern crate midir;
 
-use std::io::{stdin, stdout, Write, Read};
 use std::error::Error;
+use std::io::{stdin, stdout, Read, Write};
 
-use midir::{MidiInput, Ignore};
+use midir::{Ignore, MidiInput};
+use std::fs::File;
 use std::thread;
 use std::time::Duration;
-use std::fs::File;
 
 // use std::sync::mutex;
 
 fn main() {
-    let mut ofile = File::create("info.txt").expect("unable to create file"); 
+    let mut ofile = File::create("info.txt").expect("unable to create file");
     ofile.write_all("".as_bytes()).expect("unable to write");
-    
+
     // let mut active_notes:Vec<i32> = Vec::new();
     {
         match run() {
             Ok(_) => (),
-            Err(err) => println!("Error: {}", err)
+            Err(err) => println!("Error: {}", err),
         }
     }
-    
 }
-
 
 fn run() -> Result<(), Box<dyn Error>> {
     let mut input = String::new();
-    
+
     let mut midi_in = MidiInput::new("midir reading input")?;
     midi_in.ignore(Ignore::None);
-    
+
     // Get an input port (read from console if multiple are available)
     let in_ports = midi_in.ports();
     let in_port = match in_ports.len() {
         0 => return Err("no input port found".into()),
         1 => {
-            println!("Choosing the only available input port: {}", midi_in.port_name(&in_ports[0]).unwrap());
+            println!(
+                "Choosing the only available input port: {}",
+                midi_in.port_name(&in_ports[0]).unwrap()
+            );
             &in_ports[0]
-        },
+        }
         _ => {
             println!("\nAvailable input ports:");
             for (i, p) in in_ports.iter().enumerate() {
@@ -48,55 +49,60 @@ fn run() -> Result<(), Box<dyn Error>> {
             stdout().flush()?;
             let mut input = String::new();
             stdin().read_line(&mut input)?;
-            in_ports.get(input.trim().parse::<usize>()?)
-            .ok_or("invalid input port selected")?
+            in_ports
+                .get(input.trim().parse::<usize>()?)
+                .ok_or("invalid input port selected")?
         }
     };
-    
+
     println!("\nOpening connection");
     let in_port_name = midi_in.port_name(in_port)?;
-    
-    let mut active_notes:Vec<i32> = Vec::new();
-    let mut active_notes2:Vec<i32> = Vec::new();
+
+    let mut active_notes: Vec<i32> = Vec::new();
+    let mut active_notes2: Vec<i32> = Vec::new();
     // t
 
     // thread::spawn(move|| {
     //     loop {
     //         thread::sleep(Duration::from_millis(50));
     //         // println!("");
-             
+
     //         // println!("{:?}",active_notes);
     //     }
     // });
     // loop{
-        // active_notes = active_notes.clone();
-        // _conn_in needs to be a named parameter, because it needs to be kept alive until the end of the scope
-        let _conn_in = midi_in.connect(in_port, "midir-read-input", move |stamp, message, _| {
-            if message.len() == 3usize{
-                
+    // active_notes = active_notes.clone();
+    // _conn_in needs to be a named parameter, because it needs to be kept alive until the end of the scope
+    let _conn_in = midi_in.connect(
+        in_port,
+        "midir-read-input",
+        move |stamp, message, _| {
+            if message.len() == 3usize {
                 //println!("{:?}", message);
                 // active_notes2 = active_notes.clone();
-                if message[1] != 1{
-                    if message[1] != 64{    
-                        handle_note(message[1].into(),&mut active_notes);
+                if message[1] != 1 {
+                    if message[1] != 64 {
+                        handle_note(message[1].into(), &mut active_notes);
                         write_notes_to_file(&active_notes);
                     }
-                }
-                else {
+                } else {
                     display_board(&active_notes);
                     // println!("{:?}",active_notes);
                 }
             }
-        
-        }, ())?;
-        // let t = _conn_in.close();
-        // active_notes = active_notes.clone();
-        // println!("{:?}",t.1);
-    
+        },
+        (),
+    )?;
+    // let t = _conn_in.close();
+    // active_notes = active_notes.clone();
+    // println!("{:?}",t.1);
+
     // }
-        
-    
-    println!("Connection open, reading input from '{}' (press enter to exit) ...", in_port_name);
+
+    println!(
+        "Connection open, reading input from '{}' (press enter to exit) ...",
+        in_port_name
+    );
 
     input.clear();
     stdin().read_line(&mut input)?; // wait for next enter key press
@@ -105,17 +111,17 @@ fn run() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn handle_note(note:i32, act_notes:&mut Vec<i32>){
+fn handle_note(note: i32, act_notes: &mut Vec<i32>) {
     // println!("{}",note);
     let mut had_note = false;
-    for i in 0..act_notes.len(){
-        if act_notes[i] == note{
+    for i in 0..act_notes.len() {
+        if act_notes[i] == note {
             act_notes.remove(i);
             had_note = true;
             break;
         }
     }
-    if had_note == false{
+    if had_note == false {
         act_notes.push(note);
     }
     // println!("{:?}",act_notes);
@@ -124,29 +130,27 @@ fn handle_note(note:i32, act_notes:&mut Vec<i32>){
     // }
 }
 
-fn display_board(act_notes:&Vec<i32>){
-    for i in 21..=108{
-        if act_notes.contains(&(i as i32)){
+fn display_board(act_notes: &Vec<i32>) {
+    for i in 21..=108 {
+        if act_notes.contains(&(i as i32)) {
             print!("X");
             // print!("   ")
-        }
-        else {
+        } else {
             print!(" ");
         }
     }
     println!();
     // write_notes_to_file(act_notes);
 }
-fn write_notes_to_file(act_notes:&Vec<i32>){
-    let mut out:String=String::new();
-        
-    for note in act_notes{
+fn write_notes_to_file(act_notes: &Vec<i32>) {
+    let mut out: String = String::new();
+
+    for note in act_notes {
         // out_notes.push(note.to_string());
         out.push_str(&note.to_string());
         out.push_str("\n");
     }
 
-    let mut ofile = File::create("info.txt").expect("unable to create file"); 
+    let mut ofile = File::create("info.txt").expect("unable to create file");
     ofile.write_all(out.as_bytes()).expect("unable to write");
-
 }
--- nannou_test/src/main.rs
+++ nannou_test/src/main.rs
@@ -1,9 +1,5 @@
 use nannou::prelude::*;
-use std::fs::File;
-use std::io::Write;
-use std::env;
 use std::fs;
-use std::str::FromStr;
 
 fn main() {
     // let info = GlobalInfo{frame:10,notes:Vec::new};
@@ -12,51 +8,61 @@ fn main() {
 
 fn view(app: &App, _model: &Model, frame: Frame) {
     let draw = app.draw();
-    
+
     let win = app.window_rect();
     // Begin drawing
     // Clear the background to blue.
     draw.background().color(BLACK);
     let t = app.time;
-    
+
     // for note in &_model.keys{
     //     draw.rect()
     //     .x_y((note.note as f32 * 10.0) - (win.w() / 2.0) - 200.0, note.y)
     //     .w(8.0).h(7.0)
     //     .hsv(note.note as f32 / 70.0, 1.0, 1.0);
     // }
-    for note in &_model.keys{
+    for note in &_model.keys {
         draw.rect()
-        .x_y((note.note as f32 * win.w()/(108.0 - 21.0)) - (win.w()/2.0) - (21.0 * win.w()/(108.0 - 21.0)), note.y)
-        .w((win.w()/(108.0 - 21.0))-2.0).h(7.0)
-        .hsv(note.note as f32 / 70.0, 1.0, 1.0);
+            .x_y(
+                (note.note as f32 * win.w() / (108.0 - 21.0))
+                    - (win.w() / 2.0)
+                    - (21.0 * win.w() / (108.0 - 21.0)),
+                note.y,
+            )
+            .w((win.w() / (108.0 - 21.0)) - 2.0)
+            .h(7.0)
+            .hsv(note.note as f32 / 70.0, 1.0, 1.0);
     }
 
     draw.to_frame(app, &frame).unwrap();
 }
 // #[derive(PartialEq)]
-struct Note{
-    note:i8,
+struct Note {
+    note: i8,
     // x:f32,
-    y:f32
+    y: f32,
 }
-impl Note{
-    fn new(n:i8,y:f32)->Self{
-        Self { note: n, y: y}
+impl Note {
+    fn new(n: i8, y: f32) -> Self {
+        Self { note: n, y: y }
     }
-    fn update(&mut self){
+    fn update(&mut self) {
         self.y += 5.0;
     }
 }
 
-struct Model{
+struct Model {
     _window: window::Id,
     keys: Vec<Note>,
-    frame:i128,
+    frame: i128,
 }
-impl Model{
-    fn new(_window: window::Id)->Self{
-        Self { _window: _window, keys: Vec::new() ,frame:0}
+impl Model {
+    fn new(_window: window::Id) -> Self {
+        Self {
+            _window: _window,
+            keys: Vec::new(),
+            frame: 0,
+        }
     }
 }
 
@@ -65,43 +71,40 @@ fn model(app: &App) -> Model {
     return Model::new(_window);
 }
 
-
 fn update(app: &App, model: &mut Model, _update: Update) {
-//    _model.
+    //    _model.
     model.frame += 1;
     //if model.frame % 2 == 0
     {
-
-        
         let win = app.window_rect();
-        
-        let contents = fs::read_to_string("../midi/info.txt")
-        .expect("Something went wrong reading the file");
-        
-        let mut notes_string:Vec<&str> = contents.split("\n").collect();
+
+        let contents =
+            fs::read_to_string("../midi/info.txt").expect("Something went wrong reading the file");
+
+        let mut notes_string: Vec<&str> = contents.split("\n").collect();
         // let mut info = Info::new();
-        
+
         notes_string.pop();
-            
-        println!("{:?}",notes_string);
-        
-        for n in notes_string{
-        model.keys.push(Note::new(n.parse().unwrap(),-win.h()/2.0));
-        // println!("gut {}", n);
-    }
-    let mut deleted = 0;
-    for i in 0..model.keys.len(){
-        // note.update();
-        if model.keys[i-deleted].y > 1000.0{        
-            if model.keys.len() > 1{
-                model.keys.remove(0);
-                deleted+=1;
+
+        println!("{:?}", notes_string);
+
+        for n in notes_string {
+            model
+                .keys
+                .push(Note::new(n.parse().unwrap(), -win.h() / 2.0));
+            // println!("gut {}", n);
+        }
+        let mut deleted = 0;
+        for i in 0..model.keys.len() {
+            // note.update();
+            if model.keys[i - deleted].y > 1000.0 {
+                if model.keys.len() > 1 {
+                    model.keys.remove(0);
+                    deleted += 1;
+                }
+            } else {
+                model.keys[i - deleted].update();
             }
         }
-        else{
-            model.keys[i-deleted].update();
-        }
-        
-    }
     }
 }
