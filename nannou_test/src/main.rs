e829fc7ed635310d61087533b68bbeb4bf97154a
Max Berliński <maxber.gdynia@gmail.com>
2022-07-01 22:33:42 +0200

    settings struct sceleton and (optional) alpha particles

--- nannou_test/src/main.rs
+++ nannou_test/src/main.rs
@@ -1,13 +1,14 @@
 use nannou::prelude::*;
 use std::fs;
 
-const NOTE_SPEED: f32 = 5.0;      // speed of floating notes
-const STARTING_NOTE: i32 = 21;    // the note value of the first note on your midi device
-const ENDING_NOTE: i32 = 108;     // the note value of the last note on your midi device
-const NOTE_MARGIN: f32 = 2.0;     // margin between notes
-const WIDTH_ADJUST: bool = true;  // if false the notes are going to have a fixed width if true it will adjust to the window width
-const NOTE_WIDTH: f32 = 10.0;     // applies if WIDTH_ADJUST is set to false
-// const PEDAL_NOTE:i8 = 64;         // change this for your pedal note so the program ignores it
+const NOTE_SPEED: f32 = 5.0; // speed of floating notes
+const STARTING_NOTE: i32 = 21; // the note value of the first note on your midi device
+const ENDING_NOTE: i32 = 108; // the note value of the last note on your midi device
+const NOTE_MARGIN: f32 = 2.0; // margin between notes
+const WIDTH_ADJUST: bool = true; // if false the notes are going to have a fixed width if true it will adjust to the window width
+const NOTE_WIDTH: f32 = 10.0; // applies if WIDTH_ADJUST is set to false
+// const PEDAL_NOTE:i8 = 64; // change this for your pedal note so the program ignores it
+const USE_PARTICLES: bool = false; // Particles
 
 fn main() {
     nannou::app(model).simple_window(view).update(update).run();
@@ -39,6 +40,19 @@ fn view(app: &App, _model: &Model, frame: Frame) {
             .h(7.0)
             .hsv(note.note as f32 / 70.0, 1.0, 1.0);
     }
+    if USE_PARTICLES == true {
+        for particle in &_model.particles {
+            draw.ellipse()
+                .x_y(
+                    (particle.x * note_multiplier)
+                        - (win.w() / 2.0)
+                        - STARTING_NOTE as f32 * note_multiplier,
+                    particle.y,
+                )
+                .w(5.0)
+                .h(5.0);
+        }
+    }
 
     draw.to_frame(app, &frame).unwrap();
 }
@@ -55,11 +69,48 @@ impl Note {
         self.y += NOTE_SPEED;
     }
 }
-
+struct Settings {
+    note_speed: f32,
+    starting_note: i32,
+    ending_note: i32,
+    note_margin: f32,
+    use_width_adjust: bool,
+    note_width: f32,
+    use_particles: bool,
+    theme: NoteThemes,
+}
+#[derive(PartialEq)]
+enum NoteThemes {
+    RainbowHorizontal,
+    Classic,
+    RainbowVertical,
+}
+struct Particle {
+    x: f32,
+    y: f32,
+    lifetime: f32,
+}
+impl Particle {
+    fn new(note: &Note) -> Self {
+        Particle {
+            x: note.note as f32,
+            y: note.y,
+            lifetime: 0.0,
+        }
+    }
+}
+impl Particle {
+    fn update(&mut self) {
+        self.x += 0.3;
+        self.lifetime += 1.0;
+    }
+}
 struct Model {
     _window: window::Id,
     keys: Vec<Note>,
     frame: i128,
+    particles: Vec<Particle>,
+    // settings: Settings,
 }
 impl Model {
     fn new(_window: window::Id) -> Self {
@@ -67,6 +118,7 @@ impl Model {
             _window: _window,
             keys: Vec::new(),
             frame: 0,
+            particles: Vec::new(),
         }
     }
 }
@@ -104,6 +156,24 @@ fn update(app: &App, model: &mut Model, _update: Update) {
             }
         } else {
             model.keys[i - deleted].update();
+            if USE_PARTICLES == true {
+                // if model.frame % 2 == 0{
+                model
+                    .particles
+                    .push(Particle::new(&model.keys[i - deleted]))
+                // }
+            }
+        }
+    }
+    if USE_PARTICLES == true {
+        deleted = 0;
+        for i in 0..model.particles.len() {
+            if model.particles[i - deleted].lifetime > 5.0 {
+                model.particles.pop();
+                deleted += 1;
+            } else {
+                model.particles[i - deleted].update();
+            }
         }
     }
 }
